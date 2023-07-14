c207962139e131a04a2a3c71926f71b485528b06
Rattana <rattana.pheak.rp@gmail.com>
2023-07-14 12:09:34 +0200

    Part 2
    Part 2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,16 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $questions = [
+            [
+            'title' => 'Je suis une super question',
+            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatum harum quo similique neque quam, adipisci tenetur facere. Odio ducimus tenetur distinctio in accusantium beatae fugit vel blanditiis omnis aspernatur!'
+            ]
+        ];
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'questions' => $questions
         ]);
     }
 }
---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\QuestionType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController extends AbstractController
+{
+    #[Route('/question/ask', name: 'question_form')]
+    public function index(Request $request): Response
+    {
+        $formQuestion = $this->createForm(QuestionType::class);
+
+        $formQuestion->handleRequest($request);
+
+        if ($formQuestion->isSubmitted() && $formQuestion->isValid())
+        {
+            dump($formQuestion->getData());
+        }
+
+        return $this->render('question/index.html.twig', [
+            'controller_name' => 'QuestionController',
+            'form' => $formQuestion->createView()
+        ]);
+    }
+}
