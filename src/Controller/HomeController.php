b717a618df155631a2b587a98da0ea2968d98c54
Rattana <rattana.pheak.rp@gmail.com>
2023-07-14 13:24:03 +0200

    Form
    Form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,7 +14,33 @@ class HomeController extends AbstractController
         $questions = [
             [
             'title' => 'Je suis une super question',
-            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatum harum quo similique neque quam, adipisci tenetur facere. Odio ducimus tenetur distinctio in accusantium beatae fugit vel blanditiis omnis aspernatur!'
+            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatum harum quo similique neque quam, adipisci tenetur facere. Odio ducimus tenetur distinctio in accusantium beatae fugit vel blanditiis omnis aspernatur!',
+            'rating' => 20,
+            'author' => [
+                'name' => 'Mathilde Blabla',
+                'avatar' => 'https://randomuser.me/api/portraits/women/82.jpg'
+            ],
+            'nbrOfResponse' => 15
+        ],
+        [
+            'title' => 'Je suis une super question',
+            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatum harum quo similique neque quam, adipisci tenetur facere. Odio ducimus tenetur distinctio in accusantium beatae fugit vel blanditiis omnis aspernatur!',
+            'rating' => 0,
+            'author' => [
+                'name' => 'Camila Toto',
+                'avatar' => 'https://randomuser.me/api/portraits/women/32.jpg'
+            ],
+            'nbrOfResponse' => 15
+        ],
+        [
+            'title' => 'Je suis une super question',
+            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatum harum quo similique neque quam, adipisci tenetur facere. Odio ducimus tenetur distinctio in accusantium beatae fugit vel blanditiis omnis aspernatur!',
+            'rating' => -15,
+            'author' => [
+                'name' => 'Solene Denie',
+                'avatar' => 'https://randomuser.me/api/portraits/women/57.jpg'
+            ],
+            'nbrOfResponse' => 15
             ]
         ];
 
