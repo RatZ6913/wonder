a941f3eb6d2e2ce90a5e9cb53cd71e5867e782fa
Rattana <rattana.pheak.rp@gmail.com>
2023-07-26 12:53:26 +0200

    Signup checkout
    Signup checkout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -34,10 +35,19 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/login', name: 'login')]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('security/index.html.twig', [
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $username = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
             'controller_name' => 'SecurityController',
+            'error' => $error,
+            'username' => $username
         ]);
     }
 
